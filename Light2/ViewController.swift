092ec4aa2df84fa5e1a377aab5e4fba55c59fd0d
Ali <alirezamonfared813@gmail.com>
2023-05-23 06:22:29 -0700

    ss

--- Light2/ViewController.swift
+++ Light2/ViewController.swift
@@ -11,9 +11,33 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    updateUI()
     // Do any additional setup after loading the view.
   }
+  
+  var lighton = true
+  @IBOutlet var lightButton: UIButton!
+  
+//  func updateUI() {
+//    view.backgroundColor = lightOn ? .white : .black
+//  }
+  
 
-
+  fileprivate func updateUI() {
+    if lighton {
+      view.backgroundColor = .white
+      lightButton.setTitle("Off", for: .normal)
+    }
+    else {
+      view.backgroundColor = .black
+      lightButton.setTitle("On", for: .normal)
+    }
+  }
+  
+  @IBAction func ButtonOff(_ sender: UIButton) {
+    lighton.toggle()
+    updateUI()
+  }
+  
 }
 
